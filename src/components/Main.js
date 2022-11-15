148145da4508ff2f40f4298cf386c91f023b81d4
The-SaqlainNawaz <pacsaqiirr@gmail.com>
2022-11-15 16:58:25 +0500

    Intializing

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+
+export default function Main() {
+    return (
+        <div className="main">
+            <div className="main_game">
+                <div className="box-1 box"></div>
+            </div>
+        </div>
+
+    )
+
+}
